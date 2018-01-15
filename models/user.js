1b837b8c0415e6098016702bbadae62cc5d15e1d
William Graham <w.m.graham24@gmail.com>
2018-01-15 09:56:16 +0000

    added hash pasword

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
@@ -8,6 +9,16 @@ const userSchema = new mongoose.Schema({
   country: String,
   numberPosts: Number,
   numberThanks: Number
+});
+
+userSchema
+  .virtual
+
+userSchema.pre('save', function hasPassword(next) {
+  if(this.isModified('password')) {
+    this.password = bcrypt.hashSync(this.password, bcrypt.genSaltSync(8));
+  }
+  next();
 });
 
 module.exports = mongoose.model('User', userSchema);
