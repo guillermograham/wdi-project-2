41a0dedf4cb080a35dbcca93bac338a3c77a7258
William Graham <w.m.graham24@gmail.com>
2018-01-17 21:28:37 +0000

    fixed latLng bug on add bar form

--- src/js/app.js
+++ src/js/app.js
@@ -2,10 +2,17 @@
 $(() => {
   const $map = $('.map');
   let map = null;
+  let marker = null;
 
-  if($('.showBar').length !== 0) {
+  if($('.showBar').length !== 0 || $('.newBar').length !== 0) {
     initMap();
+
+    if(!$('.showBar').length) {
+      google.maps.event.addListener(map, 'click', handleMapClick);
+    }
   }
+
+
 
 
 
@@ -17,4 +24,32 @@ $(() => {
       center: latLng
     });
   }
+
+  function handleMapClick(event) {
+    const lat = event.latLng.lat();
+    const lng = event.latLng.lng();
+    console.log(lng);
+    console.log(lat);
+    $('input[name="address[lng]"]').val(lng);
+    $('input[name="address[lat]"]').val(lat);
+
+
+    if(marker) marker.setMap(null);
+
+    if($('#lat').val() && $('#lng').val()) {
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(lat, lng),
+        map: map
+      });
+    } else {
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(lat, lng),
+        map: map
+      });
+    }
+  }
+
+
+
+
 });
