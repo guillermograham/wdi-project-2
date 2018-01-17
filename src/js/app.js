cbd18d4d06c85823bc51fc25b8dc90a035652b07
William Graham <w.m.graham24@gmail.com>
2018-01-17 18:23:26 +0000

    added google map api to show bars page

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,20 @@
-// client side js here
-console.log('JS loaded');
+/* global google:ignore */
+$(() => {
+  const $map = $('.map');
+  let map = null;
+
+  if($('.showBar').length !== 0) {
+    initMap();
+  }
+
+
+
+  function initMap() {
+    console.log('function reached');
+    const latLng = { lat: 41.3851, lng: 2.1734 };
+    map = new google.maps.Map($map.get(0), {
+      zoom: 14,
+      center: latLng
+    });
+  }
+});
